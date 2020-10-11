4cb55b5c4317b325eb19d7997e923c5431c7ef60
Jonas Braathen <jonas@ponas.no>
2020-10-11 21:50:19 +0200

    Remove mode universal and set ssr to true

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,9 @@ import gql from 'graphql-tag'
 export default {
   /*
   ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
+  ** See https://nuxtjs.org/guides/configuration-glossary/configuration-ssr
   */
-  mode: 'universal',
+  ssr: true,
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
